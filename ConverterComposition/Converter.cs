0a447bc7187d43f061b87f7c11c8c99fb731a318
marchanyanehu <dadada.marchan@gmail.com>
2023-06-30 22:31:12 +0300

    done

--- ConverterComposition/Converter.cs
+++ ConverterComposition/Converter.cs
@@ -9,6 +9,8 @@ namespace ConverterComposition
     /// </summary>
     public class Converter
     {
+        private readonly CharsDictionary charsDictionary;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="Converter"/> class.
         /// </summary>
@@ -16,7 +18,12 @@ namespace ConverterComposition
         /// <exception cref="System.ArgumentNullException">Thrown when dictionary factory is null.</exception>
         public Converter(ICharsDictionaryFactory? dictionaryFactory)
         {
-            throw new NotImplementedException();
+            if (dictionaryFactory == null)
+            {
+                throw new ArgumentNullException(nameof(dictionaryFactory));
+            }
+
+            this.charsDictionary = dictionaryFactory.CreateDictionary();
         }
 
         /// <summary>
@@ -26,7 +33,92 @@ namespace ConverterComposition
         /// <returns>A number string representation.</returns>
         public string Convert(double number)
         {
-            throw new NotImplementedException();
+            if (this.charsDictionary.Dictionary == null)
+            {
+                throw new InvalidOperationException("CharsDictionary.Dictionary is null.");
+            }
+
+            if (number == double.Epsilon)
+            {
+                if (this.charsDictionary.Dictionary.TryGetValue(Character.Epsilon, out var word))
+                {
+                    return word;
+                }
+                else
+                {
+                    throw new InvalidOperationException("Character Epsilon not found in CharsDictionary.");
+                }
+            }
+
+            if (double.IsNegativeInfinity(number))
+            {
+                if (this.charsDictionary.Dictionary.TryGetValue(Character.NegativeInfinity, out var word))
+                {
+                    return word;
+                }
+                else
+                {
+                    throw new InvalidOperationException("Character NegativeInfinity not found in CharsDictionary.");
+                }
+            }
+
+            if (double.IsNaN(number))
+            {
+                if (this.charsDictionary.Dictionary.TryGetValue(Character.NaN, out var word))
+                {
+                    return word;
+                }
+                else
+                {
+                    throw new InvalidOperationException("Character NaN not found in CharsDictionary.");
+                }
+            }
+
+            var numberString = number.ToString(CultureInfo.CreateSpecificCulture(this.charsDictionary.CultureName ?? "en-US"));
+            var stringBuilder = new StringBuilder();
+
+            foreach (var c in numberString)
+            {
+                if (this.charsDictionary.Dictionary.TryGetValue(GetCharacterFromChar(c), out var word))
+                {
+                    if (stringBuilder.Length > 0)
+                    {
+                        stringBuilder.Append(' '); // Add a space between words
+                    }
+
+                    stringBuilder.Append(word);
+                }
+                else
+                {
+                    throw new InvalidOperationException($"Character '{c}' not found in CharsDictionary.");
+                }
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        private static Character GetCharacterFromChar(char c)
+        {
+            return c switch
+            {
+                '0' => Character.Zero,
+                '1' => Character.One,
+                '2' => Character.Two,
+                '3' => Character.Three,
+                '4' => Character.Four,
+                '5' => Character.Five,
+                '6' => Character.Six,
+                '7' => Character.Seven,
+                '8' => Character.Eight,
+                '9' => Character.Nine,
+                '+' => Character.Plus,
+                '-' => Character.Minus,
+                '.' => Character.Point,
+                ',' => Character.Comma,
+                'E' => Character.Exponent,
+                '∞' => Character.PositiveInfinity,
+                _ => throw new ArgumentException($"Invalid character '{c}'"),
+            };
         }
     }
 }
